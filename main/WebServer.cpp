3a69a3eaf62327afb786fb0e99958e6444f4295b
Nils Weigel <nils.weigel@gmail.com>
2019-11-01 17:21:03 +0100

    refactored code duplication, added get/post mode

--- main/WebServer.cpp
+++ main/WebServer.cpp
@@ -5,139 +5,99 @@
 
 static const char *WEBSERVER_LOG_TAG = "WebServer";
 
-static esp_err_t system_info_get_handler(httpd_req_t *req)
-{
-    auto *server = (WebServer*)req->user_ctx;
-    if (!server) {
-        return ESP_FAIL;
+#define CREATE_FUNCTION_TO_METHOD(fnName, handler) \
+    static esp_err_t fnName(httpd_req_t *req) \
+    { \
+        auto *server = (WebServer*)req->user_ctx; \
+        if (!server) { \
+            return ESP_FAIL; \
+        } \
+        return server->handler(req); \
     }
 
-    return server->getSystemInfo(req);
-}
+CREATE_FUNCTION_TO_METHOD(system_info_get_handler, getSystemInfo);
+CREATE_FUNCTION_TO_METHOD(led_state_get_handler, getLedState);
+CREATE_FUNCTION_TO_METHOD(led_power_get_handler, getLedPower);
+CREATE_FUNCTION_TO_METHOD(led_power_post_handler, postLedPower);
+CREATE_FUNCTION_TO_METHOD(led_mode_get_handler, getLedMode);
+CREATE_FUNCTION_TO_METHOD(led_mode_post_handler, postLedMode);
+CREATE_FUNCTION_TO_METHOD(led_modes_get_handler, getLedModes);
 
-static esp_err_t led_state_get_handler(httpd_req_t *req)
-{
-    auto *server = (WebServer*)req->user_ctx;
-    if (!server) {
-        return ESP_FAIL;
-    }
-
-    return server->getLedState(req);
-}
-
-static esp_err_t led_power_get_handler(httpd_req_t *req)
-{
-    auto *server = (WebServer*)req->user_ctx;
-    if (!server) {
-        return ESP_FAIL;
-    }
-
-    return server->getLedPower(req);
-}
-static esp_err_t led_power_post_handler(httpd_req_t *req)
-{
-    auto *server = (WebServer*)req->user_ctx;
-    if (!server) {
-        return ESP_FAIL;
-    }
-
-    return server->postLedPower(req);
-}
-
-WebServer::WebServer(LedController *ledController):
-    m_ledController(ledController)
-{
-}
+WebServer::WebServer(LedController *ledController):m_ledController(ledController) {}
 
 esp_err_t WebServer::getSystemInfo(httpd_req_t *req)
 {
-    httpd_resp_set_type(req, "application/json");
     cJSON *root = cJSON_CreateObject();
+    cJSON_AddStringToObject(root, "version", IDF_VER);
+
     esp_chip_info_t chip_info;
     esp_chip_info(&chip_info);
-    cJSON_AddStringToObject(root, "version", IDF_VER);
     cJSON_AddNumberToObject(root, "cores", chip_info.cores);
 
-    const char *sys_info = cJSON_Print(root);
-    httpd_resp_sendstr(req, sys_info);
-
-    free((void *)sys_info);
-    cJSON_Delete(root);
-
-    return ESP_OK;
+    return jsonResponse(root, req);
 }
 
 esp_err_t WebServer::getLedState(httpd_req_t *req)
 {
-    httpd_resp_set_type(req, "application/json");
     cJSON *root = cJSON_CreateObject();
-
     cJSON_AddNumberToObject(root, "power", m_ledController->getPower());
     cJSON_AddNumberToObject(root, "mode", m_ledController->getMode());
 
-    const char *sys_info = cJSON_Print(root);
-    httpd_resp_sendstr(req, sys_info);
-
-    free((void *)sys_info);
-    cJSON_Delete(root);
-
-    return ESP_OK;
+    return jsonResponse(root, req);
 }
 
 esp_err_t WebServer::getLedPower(httpd_req_t *req)
 {
-    httpd_resp_set_type(req, "application/json");
     cJSON *root = cJSON_CreateObject();
-
     cJSON_AddNumberToObject(root, "power", m_ledController->getPower());
 
-    const char *sys_info = cJSON_Print(root);
-    httpd_resp_sendstr(req, sys_info);
-
-    free((void *)sys_info);
-    cJSON_Delete(root);
-
-    return ESP_OK;
+    return jsonResponse(root, req);
 }
 
 esp_err_t WebServer::postLedPower(httpd_req_t *req)
 {
-    int total_len = req->content_len;
-    int cur_len = 0;
-    char *buf = m_readBuffer;
-    int received = 0;
-    if (total_len >= READ_BUFFER_SIZE) {
-        /* Respond with 500 Internal Server Error */
-        httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR, "content too long");
-        return ESP_FAIL;
-    }
-    while (cur_len < total_len) {
-        received = httpd_req_recv(req, buf + cur_len, total_len);
-        if (received <= 0) {
-            /* Respond with 500 Internal Server Error */
-            httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR, "Failed to post control value");
-            return ESP_FAIL;
-        }
-        cur_len += received;
-    }
-    buf[total_len] = '\0';
-
-    cJSON *root = cJSON_Parse(buf);
-    if (!root) {
-        httpd_resp_send_err(req, HTTPD_400_BAD_REQUEST, "Failed to parse JSON");
-    } else {
+    return handlePost(req, [=](cJSON *root) -> bool {
         cJSON *const powerObj = cJSON_GetObjectItem(root, "power");
-        if (powerObj) {
-            int power = powerObj->valueint;
-            m_ledController->setPower(power);
-        } else {
-            httpd_resp_send_err(req, HTTPD_400_BAD_REQUEST, "Missing item \"power\"");
+        if (!powerObj) {
+            return false;
         }
-    }
 
-    cJSON_Delete(root);
-    httpd_resp_sendstr(req, "Post control value successfully");
-    return ESP_OK;
+        int power = powerObj->valueint;
+        m_ledController->setPower(power);
+        return true;
+    });
+}
+
+esp_err_t WebServer::getLedMode(httpd_req_t *req)
+{
+    cJSON *root = cJSON_CreateObject();
+    cJSON_AddNumberToObject(root, "mode", m_ledController->getMode());
+
+    return jsonResponse(root, req);
+}
+
+esp_err_t WebServer::postLedMode(httpd_req_t *req)
+{
+    return handlePost(req, [=](cJSON *root) -> bool {
+        cJSON *const modeObj = cJSON_GetObjectItem(root, "mode");
+        if (!modeObj) {
+            return false;
+        }
+
+        int mode = modeObj->valueint;
+        m_ledController->setMode((LedController::Mode)mode);
+        return true;
+    });
+}
+
+esp_err_t WebServer::getLedModes(httpd_req_t *req)
+{
+    // TODO array of mode objects (num, name)
+
+    cJSON *root = cJSON_CreateObject();
+    cJSON_AddNumberToObject(root, "modes", 1337);
+
+    return jsonResponse(root, req);
 }
 
 void WebServer::startServer()
@@ -196,4 +156,75 @@ void WebServer::registerUriHandlers()
             .user_ctx = this
     };
     httpd_register_uri_handler(m_hdnlServer, &led_power_post_uri);
+
+    httpd_uri_t led_mode_get_uri = {
+            .uri = "/api/v1/led/mode",
+            .method = HTTP_GET,
+            .handler = led_mode_get_handler,
+            .user_ctx = this
+    };
+    httpd_register_uri_handler(m_hdnlServer, &led_mode_get_uri);
+    httpd_uri_t led_mode_post_uri = {
+            .uri = "/api/v1/led/mode",
+            .method = HTTP_POST,
+            .handler = led_mode_post_handler,
+            .user_ctx = this
+    };
+    httpd_register_uri_handler(m_hdnlServer, &led_mode_post_uri);
+    httpd_uri_t led_modes_get_uri = {
+            .uri = "/api/v1/led/modes",
+            .method = HTTP_GET,
+            .handler = led_modes_get_handler,
+            .user_ctx = this
+    };
+    httpd_register_uri_handler(m_hdnlServer, &led_modes_get_uri);
+}
+
+esp_err_t WebServer::jsonResponse(cJSON *root, httpd_req_t *req)
+{
+    httpd_resp_set_type(req, "application/json");
+
+    const char *responsePayload = cJSON_Print(root);
+    httpd_resp_sendstr(req, responsePayload);
+
+    free((void *)responsePayload);
+    cJSON_Delete(root);
+
+    return ESP_OK;
+}
+
+esp_err_t WebServer::handlePost(httpd_req_t *req, const std::function<bool(cJSON *root)>& jsonHandler)
+{
+    int total_len = req->content_len;
+    int cur_len = 0;
+    char *buf = m_readBuffer;
+    int received = 0;
+    if (total_len >= READ_BUFFER_SIZE) {
+        /* Respond with 500 Internal Server Error */
+        httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR, "content too long");
+        return ESP_FAIL;
+    }
+    while (cur_len < total_len) {
+        received = httpd_req_recv(req, buf + cur_len, total_len);
+        if (received <= 0) {
+            /* Respond with 500 Internal Server Error */
+            httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR, "Failed to post control value");
+            return ESP_FAIL;
+        }
+        cur_len += received;
+    }
+    buf[total_len] = '\0';
+
+    cJSON *root = cJSON_Parse(buf);
+    if (!root) {
+        httpd_resp_send_err(req, HTTPD_400_BAD_REQUEST, "Failed to parse JSON");
+    } else {
+        if (!jsonHandler(root)) {
+            httpd_resp_send_err(req, HTTPD_400_BAD_REQUEST, "JSON is missing keys or has invalid values");
+        }
+    }
+
+    cJSON_Delete(root);
+    httpd_resp_sendstr(req, "Post control value successfully");
+    return ESP_OK;
 }
--- main/WebServer.h
+++ main/WebServer.h
@@ -2,7 +2,10 @@
 #define LEDWALL_WEBSERVER_H
 
 #include <esp_http_server.h>
+#include <functional>
+
 class LedController;
+class cJSON;
 
 #define READ_BUFFER_SIZE 1024 * 5
 
@@ -15,6 +18,9 @@ public:
     esp_err_t getLedState(httpd_req_t *req);
     esp_err_t getLedPower(httpd_req_t *req);
     esp_err_t postLedPower(httpd_req_t *req);
+    esp_err_t getLedMode(httpd_req_t *req);
+    esp_err_t postLedMode(httpd_req_t *req);
+    esp_err_t getLedModes(httpd_req_t *req);
 
     void startServer();
     void stopServer();
@@ -25,6 +31,9 @@ protected:
     char m_readBuffer[READ_BUFFER_SIZE];
 
     void registerUriHandlers();
+
+    esp_err_t jsonResponse(cJSON *root, httpd_req_t *req);
+    esp_err_t handlePost(httpd_req_t *req, const std::function<bool(cJSON *root)>& jsonHandler);
 };
 
 #endif //LEDWALL_WEBSERVER_H
