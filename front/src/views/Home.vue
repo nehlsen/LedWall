e40816e3a86a081746e0777119b14edabcf4953b
Nils Weigel <nils.weigel@gmail.com>
2019-11-20 19:22:42 +0100

    playing around with some more info on the start page

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -6,11 +6,17 @@
           <v-img :src="require('../assets/logo.png')" contain height="200"></v-img>
           <v-card-title primary-title>
             <div class="ma-auto">
-              <span class="grey--text">IDF version: {{version}}</span>
+              <span class="grey--text">IDF version: {{idfVersion}}</span>
               <br>
               <span class="grey--text">ESP cores: {{cores}}</span>
               <br>
               <span class="grey--text">System restarts: {{restarts}}</span>
+              <br>
+              <span class="grey--text">Project Name: {{projectName}}</span>
+              <br>
+              <span class="grey--text">Project Version: {{projectVersion}}</span>
+              <br>
+              <span class="grey--text">Compiled: {{compileTime}} / {{compileDate}}</span>
             </div>
           </v-card-title>
         </v-card>
@@ -21,20 +27,30 @@
 
 <script>
 export default {
-  data() {
+  data () {
     return {
-      version: null,
+      sysInfo: null,
+      idfVersion: null,
       cores: null,
-      restarts: null
-    };
+      restarts: null,
+      projectName: null,
+      projectVersion: null,
+      compileDate: null,
+      compileTime: null
+    }
   },
-  mounted() {
+  mounted () {
     this.$ajax
-      .get("/api/v1/system/info")
+      .get('/api/v1/system/info')
       .then(data => {
-        this.version = data.data.version;
+        this.sysInfo = data.data;
+        this.idfVersion = data.data.idfVersion;
         this.cores = data.data.cores;
         this.restarts = data.data.restarts;
+        this.projectName = data.data.projectName;
+        this.projectVersion = data.data.projectVersion;
+        this.compileTime = data.data.compileTime;
+        this.compileDate = data.data.compileDate;
       })
       .catch(error => {
         console.log(error);
