0190c8ddaafe816c97a7cd1b3c55e396095d087f
Nils Weigel <nils.weigel@gmail.com>
2020-02-25 19:07:54 +0100

    some mdns init error checks, added mqtt to mdns

--- main/main.cpp
+++ main/main.cpp
@@ -25,22 +25,36 @@ static const char *APP_LOG_TAG = "LED_WALL";
 extern "C" {
 #endif
 
-static void initialise_mdns()
+static esp_err_t initialise_mdns()
 {
-    if (mdns_init() != ESP_OK) {
-        ESP_LOGW(APP_LOG_TAG, "mDNS init failed");
+    esp_err_t ret = mdns_init();
+    if (ret != ESP_OK) {
+        ESP_LOGW(APP_LOG_TAG, "mDNS: init failed");
+        return ret;
     }
 
-    mdns_hostname_set(CONFIG_MDNS_HOSTNAME);
-    mdns_instance_name_set("LedWall REST and Web interface");
+    ret = mdns_hostname_set(CONFIG_MDNS_HOSTNAME);
+    if (ret != ESP_OK) {
+        ESP_LOGW(APP_LOG_TAG, "mDNS: set hostname failed");
+        return ret;
+    }
 
-    mdns_txt_item_t serviceTxtData[] = {
+    ret = mdns_instance_name_set("LedWall REST and Web interface");
+    if (ret != ESP_OK) {
+        ESP_LOGW(APP_LOG_TAG, "mDNS: set instance name failed");
+        return ret;
+    }
+
+    mdns_txt_item_t httpServiceTxt[] = {
             {"board", "esp32"},
             {"path", "/"}
     };
 
-    ESP_ERROR_CHECK(mdns_service_add("LedWall-WebServer", "_http", "_tcp", 80, serviceTxtData,
-                                     sizeof(serviceTxtData) / sizeof(serviceTxtData[0])));
+    ESP_ERROR_CHECK(mdns_service_add("LedWall-WebServer", "_http", "_tcp", 80, httpServiceTxt,
+                                     sizeof(httpServiceTxt) / sizeof(httpServiceTxt[0])));
+    ESP_ERROR_CHECK(mdns_service_add("LedWall-MQTT", "_mqtt", "_tcp", 1883, {}, 0));
+
+    return ESP_OK;
 }
 
 esp_err_t init_fs(void)
