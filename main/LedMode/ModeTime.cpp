59116ebb1bf7accc82d42c2260a7ca4b97a64a86
Nils Weigel <nils.weigel@gmail.com>
2020-10-27 07:22:09 +0100

    ModeTime: initial work on other variants (Countdown, etc)

--- main/LedMode/ModeTime.cpp
+++ main/LedMode/ModeTime.cpp
@@ -1,13 +1,18 @@
+#include <cJSON.h>
+//#include <Delayer.h>
 #include "ModeTime.h"
-#include <esp_log.h>
 
-namespace LedWall {
-namespace Mode {
+namespace LedWall::Mode {
 
-ModeTime::ModeTime(LedMatrix &matrix): ModeText(matrix) {}
+ModeTime::ModeTime(LedMatrix &matrix): ModeText(matrix)
+{
+    setScrollMode(ScrollNone);
+}
 
 bool ModeTime::update()
 {
+//    EBLi::Delayer delayer;
+
     const uint16_t delay = 1000;
     int64_t currentTime = esp_timer_get_time() / 1000;
     if ((currentTime - m_lastTimeUpdate < delay) && m_lastTimeUpdate > 0) {
@@ -18,19 +23,135 @@ bool ModeTime::update()
     static bool isOdd = false;
     isOdd = !isOdd;
 
+    switch (getVariant()) {
+        case VariantTime:
+            showTime(isOdd);
+            break;
+
+        case VariantCountDown:
+            showCountDown(isOdd);
+            break;
+
+        case VariantCountUp:
+            showCountUp(isOdd);
+            break;
+
+        default:
+            // TODO
+            break;
+    }
+
+    return ModeText::update();
+}
+
+void ModeTime::readOptions(cJSON *root)
+{
+    cJSON_AddNumberToObject(root, "variant", getVariant());
+    cJSON_AddNumberToObject(root, "parameter", getVariantParameter());
+}
+
+bool ModeTime::writeOptions(cJSON *root)
+{
+    cJSON *const requestedVariant = cJSON_GetObjectItem(root, "variant");
+    if (requestedVariant) {
+        setVariant(static_cast<Variant>(requestedVariant->valueint));
+    }
+
+    cJSON *const requestedVariantParameter = cJSON_GetObjectItem(root, "parameter");
+    if (requestedVariantParameter) {
+        setVariantParameter(requestedVariantParameter->valueint);
+    }
+
+    return requestedVariant || requestedVariantParameter;
+}
+
+void ModeTime::setVariant(ModeTime::Variant variant)
+{
+    m_variant = variant;
+}
+
+ModeTime::Variant ModeTime::getVariant() const
+{
+    return m_variant;
+}
+
+void ModeTime::setVariantParameter(int variantParameter)
+{
+    m_variantParameter = variantParameter;
+}
+
+int ModeTime::getVariantParameter() const
+{
+    return m_variantParameter;
+}
+
+void ModeTime::setVariantTime()
+{
+    setVariant(Variant::VariantTime);
+}
+
+void ModeTime::setVariantCountdown(int countdownSeconds)
+{
+    setVariant(Variant::VariantCountDown);
+    setVariantParameter(countdownSeconds);
+}
+
+void ModeTime::setVariantCountUp()
+{
+    setVariant(Variant::VariantCountUp);
+    setVariantParameter(0);
+}
+
+void ModeTime::showTime(bool isOddSecond)
+{
     time_t now;
     char strftime_buf[64];
     struct tm timeinfo;
 
     time(&now);
     localtime_r(&now, &timeinfo);
-    strftime(strftime_buf, sizeof(strftime_buf), (isOdd ? "%H %M" : "%H:%M"), &timeinfo);
+    strftime(strftime_buf, sizeof(strftime_buf), (isOddSecond ? "%H %M" : "%H:%M"), &timeinfo);
 
-    setScrollMode(ScrollNone);
     setText(strftime_buf);
-
-    return ModeText::update();
 }
 
+void ModeTime::showCountDown(bool isOddSecond)
+{
+    std::string remainingTime = formatSeconds(m_variantParameter, isOddSecond);
+
+    if (m_variantParameter > 0) {
+        --m_variantParameter;
+    }
+
+    setText(remainingTime);
 }
+
+void ModeTime::showCountUp(bool isOddSecond)
+{
+    std::string timeSoFar = formatSeconds(m_variantParameter, isOddSecond);
+
+    ++m_variantParameter;
+
+    setText(timeSoFar);
+}
+
+std::string ModeTime::formatSeconds(int secondsToFormat, bool includeSplittingDots)
+{
+    std::string formattedSeconds;
+
+    int seconds = secondsToFormat % 60;
+    int minutes = (secondsToFormat-seconds) / 60;
+    // FIXME if minutes > 99
+
+    formattedSeconds += (minutes < 10 ? "0" : "");
+    formattedSeconds += std::to_string(minutes);
+
+    formattedSeconds += (includeSplittingDots ? " " : ":");
+
+    formattedSeconds += (seconds < 10 ? "0" : "");
+    formattedSeconds += std::to_string(seconds);
+
+    return formattedSeconds;
+}
+
 }
--- main/LedMode/ModeTime.h
+++ main/LedMode/ModeTime.h
@@ -3,8 +3,7 @@
 
 #include "ModeText.h"
 
-namespace LedWall {
-namespace Mode {
+namespace LedWall::Mode {
 
 class ModeTime : public ModeText
 {
@@ -13,11 +12,40 @@ public:
 
     bool update() override;
 
+    void readOptions(cJSON *root) override;
+    bool writeOptions(cJSON *root) override;
+
+    enum Variant {
+        VariantTime = 0,
+        VariantCountDown = 1, // count down from e.g. 10 Minutes
+        VariantCountDownTo = 2, // count down to e.g. 31.12. 24:00
+        VariantCountUp = 3, // just start to count time since "now"
+        VariantCountSince = 4, // time since e.g. 1.1. 0:00
+    };
+
+    void setVariant(Variant variant);
+    Variant getVariant() const;
+    void setVariantParameter(int variantParameter);
+    int getVariantParameter() const;
+
+    void setVariantTime();
+    void setVariantCountdown(int countdownSeconds);
+//    void setVariantCountdownTo();
+    void setVariantCountUp();
+//    void setVariantCountSince();
+
 private:
     int64_t m_lastTimeUpdate = 0;
+    Variant m_variant = Variant::VariantTime;
+    int m_variantParameter = 0;
+
+    void showTime(bool isOddSecond);
+    void showCountDown(bool isOddSecond);
+    void showCountUp(bool isOddSecond);
+
+    static std::string formatSeconds(int seconds, bool includeSplittingDots);
 };
 
-} // namespace Mode
-} // namespace LedWall
+}
 
 #endif //LEDWALL_MODETIME_H
